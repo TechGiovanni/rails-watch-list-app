90627d581a748f08d2f2d1ec5d15fa4ee1108df9
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-11-13 15:11:58 -0500

    Added javascript typed package

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'lists#index'
+  resources :lists, only: %i[new create show destroy edit] do
+    resources :bookmarks, only: %i[new create]
+    resources :reviews, only: %i[create]
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
