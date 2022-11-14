26982ccddfb2979f09b879138dc78671e8bc8d43
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-11-14 00:09:33 -0500

    was able to add dynamic images that change on every request

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,6 +4,11 @@ class BookmarksController < ApplicationController
 
   def new
     @bookmark = Bookmark.new
+    @banners = [
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668386585/MovieListApp/6366_i75e0x.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203187/MovieListApp/vmoqijzbotvhobjrwp4h.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203186/MovieListApp/ehmcnldlqdfndl5whlmk.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400729/MovieListApp/ERURg5kVAAAhLTC_yy8wf7.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400739/MovieListApp/mortal-kombat-2021-movies-poster-4096x1895-4902_p0oayx.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400497/MovieListApp/wtV2Jd5GP1B1VA8focKNqclqv5ERFRor9FQStCnG0-s_cwuwol.webp'
+    ]
   end
 
   def create
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,20 +8,40 @@ class ListsController < ApplicationController
       'https://res.cloudinary.com/dppymdnxh/image/upload/v1668285725/MovieListApp/card_bg_images/pexels-quark-studio-2507025_m2zqpj.jpg',
       'https://res.cloudinary.com/dppymdnxh/image/upload/v1668285729/MovieListApp/card_bg_images/pexels-claudio-siracusano-13400083_evhe7e.jpg'
     ]
+    @banners = [
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668386585/MovieListApp/6366_i75e0x.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203187/MovieListApp/vmoqijzbotvhobjrwp4h.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203186/MovieListApp/ehmcnldlqdfndl5whlmk.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400729/MovieListApp/ERURg5kVAAAhLTC_yy8wf7.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400739/MovieListApp/mortal-kombat-2021-movies-poster-4096x1895-4902_p0oayx.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400497/MovieListApp/wtV2Jd5GP1B1VA8focKNqclqv5ERFRor9FQStCnG0-s_cwuwol.webp'
+    ]
 
     @lists = List.all
   end
 
   def show
     @bookmark = Bookmark.new
-    banner
+    @banners = [
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668386585/MovieListApp/6366_i75e0x.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203187/MovieListApp/vmoqijzbotvhobjrwp4h.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203186/MovieListApp/ehmcnldlqdfndl5whlmk.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400729/MovieListApp/ERURg5kVAAAhLTC_yy8wf7.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400739/MovieListApp/mortal-kombat-2021-movies-poster-4096x1895-4902_p0oayx.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400497/MovieListApp/wtV2Jd5GP1B1VA8focKNqclqv5ERFRor9FQStCnG0-s_cwuwol.webp'
+    ]
   end
 
   def new
     @list = List.new
+    @banners = [
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668386585/MovieListApp/6366_i75e0x.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203187/MovieListApp/vmoqijzbotvhobjrwp4h.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203186/MovieListApp/ehmcnldlqdfndl5whlmk.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400729/MovieListApp/ERURg5kVAAAhLTC_yy8wf7.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400739/MovieListApp/mortal-kombat-2021-movies-poster-4096x1895-4902_p0oayx.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400497/MovieListApp/wtV2Jd5GP1B1VA8focKNqclqv5ERFRor9FQStCnG0-s_cwuwol.webp'
+    ]
   end
 
   def create
+    @banners = [
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668386585/MovieListApp/6366_i75e0x.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203187/MovieListApp/vmoqijzbotvhobjrwp4h.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203186/MovieListApp/ehmcnldlqdfndl5whlmk.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400729/MovieListApp/ERURg5kVAAAhLTC_yy8wf7.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400739/MovieListApp/mortal-kombat-2021-movies-poster-4096x1895-4902_p0oayx.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668400497/MovieListApp/wtV2Jd5GP1B1VA8focKNqclqv5ERFRor9FQStCnG0-s_cwuwol.webp'
+    ]
+
     @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list), notice: 'movie was successfully created'
@@ -43,13 +63,5 @@ class ListsController < ApplicationController
 
   def list_params
     params.require(:list).permit(:name, :photo)
-  end
-
-  def banner
-    @banners = [
-      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668386585/MovieListApp/6366_i75e0x.jpg',
-      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203187/MovieListApp/vmoqijzbotvhobjrwp4h.jpg',
-      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668203186/MovieListApp/ehmcnldlqdfndl5whlmk.jpg'
-    ]
   end
 end
